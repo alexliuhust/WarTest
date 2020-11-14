dd189ca572d05cda4ee8eec4b86f3953caa081e4
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-11-13 20:10:19 -0500

    Temporary Test package

--- WarTestManagement/src/com/wartest/test/Test.java
+++ WarTestManagement/src/com/wartest/test/Test.java
@@ -11,6 +11,72 @@ import com.wartest.util.DbUtil;
 
 public class Test {
 	
+	public ResultSet findArmById(Connection con, Integer armID) throws Exception {
+		String sql = "select * from arms where armID = ?";
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.setInt(1, armID);
+		return pstmt.executeQuery();
+		
+	}
+	public void findArmByIdTest() {
+		DbUtil dbUtil = new DbUtil();
+		Connection con = null;
+		try {
+			con = dbUtil.getCon();
+			ResultSet rs = findArmById(con, 33);
+			while(rs.next()) {
+				System.out.println(rs.getString("name") + 
+					", " + rs.getString("race") + 
+					", " + rs.getString("type"));
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				dbUtil.closeCon(con);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
+	public ResultSet findArmByRace(Connection con, String race) throws Exception {
+		String sql = "select * from arms where race = ?";
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.setString(1, race);
+		return pstmt.executeQuery();
+	}
+	public void findArmByRaceTest() {
+		DbUtil dbUtil = new DbUtil();
+		Connection con = null;
+		try {
+			con = dbUtil.getCon();
+			ResultSet rs = findArmByRace(con, "Skaven");
+			while(rs.next()) {
+				System.out.println(rs.getInt("armID") + 
+					", " + rs.getString("name") + 
+					", " + rs.getString("race") + 
+					", " + rs.getString("type"));
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				dbUtil.closeCon(con);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		System.out.println("-------------findArmByIdTest-------------");
+		test.findArmByIdTest();
+		System.out.println("-------------findArmByRaceTest-------------");
+		test.findArmByRaceTest();
+		
+	}
 
 }
