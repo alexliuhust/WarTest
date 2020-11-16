6279a0e0cb05acb65e5c822aab20683c402568cd
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-11-16 17:16:18 -0500

    LogIn Frame completed

---
+++ WarTestManagement/src/com/wartest/view/LogInFrm.java
@@ -0,0 +1,189 @@
+package com.wartest.view;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+
+import com.wartest.dao.UserDao;
+import com.wartest.model.User;
+import com.wartest.util.DbUtil;
+import com.wartest.util.StringUtil;
+
+public class LogInFrm extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField usernameTxt;
+	private JPasswordField passwordTXT;
+	
+	private DbUtil dbUtil = new DbUtil();
+	private UserDao userDao = new UserDao();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LogInFrm frame = new LogInFrm();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LogInFrm() {
+		setResizable(false);
+		setTitle("User Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 404);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Total War II War Drill System");
+		lblNewLabel.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 24));
+		
+		JLabel lblNewLabel_1 = new JLabel("Username");
+		lblNewLabel_1.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 15));
+		
+		JLabel lblNewLabel_2 = new JLabel("Password");
+		lblNewLabel_2.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 15));
+		
+		usernameTxt = new JTextField();
+		usernameTxt.setColumns(10);
+		
+		passwordTXT = new JPasswordField();
+		
+		JButton btnNewButton = new JButton("Log In");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				loginActionPerformed(e);
+			}
+		});
+		btnNewButton.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 14));
+		
+		JButton btnNewButton_1 = new JButton("Reset");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				resetValueActionPerformed(e);
+			}
+		});
+		btnNewButton_1.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 14));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(50, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 68, GroupLayout.PREFERRED_SIZE))
+									.addGap(18)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(passwordTXT)
+										.addComponent(usernameTxt, GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE))))
+							.addGap(46))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnNewButton)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(btnNewButton_1)
+							.addGap(68))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(35)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
+					.addGap(64)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(usernameTxt, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1))
+					.addGap(37)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(passwordTXT, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_2))
+					.addGap(44)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton)
+						.addComponent(btnNewButton_1))
+					.addContainerGap(71, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+		this.setLocationRelativeTo(null);
+	}
+	/**
+	 * Action Event: Log In
+	 * @param e
+	 */
+	private void loginActionPerformed(ActionEvent event) {
+		String userName = this.usernameTxt.getText();
+		String password = (new String(this.passwordTXT.getPassword()));
+		if (StringUtil.isEmpty(userName)) {
+			JOptionPane.showMessageDialog(null, "Username cannot be EMPTY!");
+			return;
+		}
+		if (StringUtil.isEmpty(password)) {
+			JOptionPane.showMessageDialog(null, "Password cannot be EMPTY!");
+			return;
+		}
+		User user = new User(userName, password);
+		Connection con = null;
+		try {
+			con = dbUtil.getCon();
+			User currentUser = userDao.login(con, user);
+			if (currentUser != null) {
+				JOptionPane.showMessageDialog(null, currentUser.getUsername() + ", Logged You In!");
+				dispose();
+				// new MainFrm().setVisible(true);
+			}else {
+				JOptionPane.showMessageDialog(null, "Incorrect username or password!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				dbUtil.closeCon(con);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Action Event: Reset Values
+	 * @param event
+	 */
+	private void resetValueActionPerformed(ActionEvent event) {
+		this.usernameTxt.setText("");
+		this.passwordTXT.setText("");
+	}
+}
+
+
+
+
