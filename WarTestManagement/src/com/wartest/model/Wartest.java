06fd5d4af0f9d0be48e1dd8e5a9a1f4e17dcd9e4
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-11-18 21:27:52 -0500

    Wartest class modified

--- WarTestManagement/src/com/wartest/model/Wartest.java
+++ WarTestManagement/src/com/wartest/model/Wartest.java
@@ -7,10 +7,11 @@ package com.wartest.model;
  */
 public class Wartest {
 	private Integer warID;
-	private Troop troop1;
-	private Troop troop2;
+	private Integer userID;
+	private Integer troop1;
+	private Integer troop2;
 	private String location;
-	private Troop victor;
+	private Integer victor;
 	private Integer arms_left;
 	
 	
@@ -18,7 +19,7 @@ public class Wartest {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Wartest(Troop troop1, Troop troop2, String location, Troop victor, Integer arms_left) {
+	public Wartest(Integer troop1, Integer troop2, String location, Integer victor, Integer arms_left) {
 		super();
 		this.troop1 = troop1;
 		this.troop2 = troop2;
@@ -26,7 +27,7 @@ public class Wartest {
 		this.victor = victor;
 		this.arms_left = arms_left;
 	}
-	public Wartest(Integer warID, Troop troop1, Troop troop2, String location, Troop victor, Integer arms_left) {
+	public Wartest(Integer warID, Integer troop1, Integer troop2, String location, Integer victor, Integer arms_left) {
 		super();
 		this.warID = warID;
 		this.troop1 = troop1;
@@ -43,16 +44,22 @@ public class Wartest {
 	public void setWarID(Integer warID) {
 		this.warID = warID;
 	}
-	public Troop getTroop1() {
+	public Integer getUserID() {
+		return userID;
+	}
+	public void setUserID(Integer userID) {
+		this.userID = userID;
+	}
+	public Integer getTroop1() {
 		return troop1;
 	}
-	public void setTroop1(Troop troop1) {
+	public void setTroop1(Integer troop1) {
 		this.troop1 = troop1;
 	}
-	public Troop getTroop2() {
+	public Integer getTroop2() {
 		return troop2;
 	}
-	public void setTroop2(Troop troop2) {
+	public void setTroop2(Integer troop2) {
 		this.troop2 = troop2;
 	}
 	public String getLocation() {
@@ -61,10 +68,10 @@ public class Wartest {
 	public void setLocation(String location) {
 		this.location = location;
 	}
-	public Troop getVictor() {
+	public Integer getVictor() {
 		return victor;
 	}
-	public void setVictor(Troop victor) {
+	public void setVictor(Integer victor) {
 		this.victor = victor;
 	}
 	public Integer getArms_left() {
@@ -75,9 +82,9 @@ public class Wartest {
 	}
 	
 	
-	@Override
-	public String toString() {
-		return "Wartest [warID=" + warID + ", troop1=" + troop1 + ", troop2=" + troop2 + ", location=" + location
-				+ ", victor=" + victor + ", arms_left=" + arms_left + "]";
-	}
+//	@Override
+//	public String toString() {
+//		return "Wartest [warID=" + warID + ", troop1=" + troop1 + ", troop2=" + troop2 + ", location=" + location
+//				+ ", victor=" + victor + ", arms_left=" + arms_left + "]";
+//	}
 }
